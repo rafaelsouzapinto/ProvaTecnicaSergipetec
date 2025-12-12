fed99f7d28b83cd45a96f951b535f63597016e0f
Antony Rafael Souza Pinto <154285174+rafaelsouzapinto@users.noreply.github.com>
2025-12-12 17:42:53 -0300

    init: first commit

---
+++ src/main/java/dev/rafael/ProvaSergipetec/ProvaSergipetecApplication.java
@@ -0,0 +1,13 @@
+package dev.rafael.ProvaSergipetec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaSergipetecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaSergipetecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rafael/ProvaSergipetec/ProvaSergipetecApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rafael.ProvaSergipetec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaSergipetecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
