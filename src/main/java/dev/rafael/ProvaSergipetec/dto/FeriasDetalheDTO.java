e4fd1842db24f9cdd56cc349d0f3521b2c2a340d
Antony Rafael Souza Pinto <154285174+rafaelsouzapinto@users.noreply.github.com>
2025-12-14 13:20:12 -0300

    feat: adicionados @JsonFormat nas datas de todos os DTOs

--- src/main/java/dev/rafael/ProvaSergipetec/dto/FeriasDetalheDTO.java
+++ src/main/java/dev/rafael/ProvaSergipetec/dto/FeriasDetalheDTO.java
@@ -1,10 +1,14 @@
 package dev.rafael.ProvaSergipetec.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDate;
 
 public record FeriasDetalheDTO(
         Long id,
+        @JsonFormat(pattern = "dd/MM/yyyy")
         LocalDate dataInicio,
+        @JsonFormat(pattern = "dd/MM/yyyy")
         LocalDate dataFim,
         String statusFerias,
         Long idServidor,
--- src/main/java/dev/rafael/ProvaSergipetec/dto/PagamentoDTO.java
+++ src/main/java/dev/rafael/ProvaSergipetec/dto/PagamentoDTO.java
@@ -1,11 +1,14 @@
 package dev.rafael.ProvaSergipetec.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record PagamentoDTO(
         Long id,
         BigDecimal valor,
+        @JsonFormat(pattern = "dd/MM/yyyy")
         LocalDate dataPagamento,
         String descricao
 ) {}
